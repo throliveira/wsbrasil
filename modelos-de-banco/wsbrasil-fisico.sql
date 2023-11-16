14ff0efb7e343366b98fc16e9025bb65f81e2714
throliveira <135062106+throliveira@users.noreply.github.com>
2023-11-16 01:17:42 -0300

    atualizações nos modelos lógico, conceitual e inicio de povoamento do modelo físico

--- modelos-de-banco/wsbrasil-fisico.sql
+++ modelos-de-banco/wsbrasil-fisico.sql
@@ -19,12 +19,13 @@ CREATE TABLE Usuario (
     fk_TipoUsuario_ID INT
 );
 
-CREATE TABLE Login (
+CREATE TABLE Login_Usuario(
     Id INT PRIMARY KEY IDENTITY(1,1),
     Email VARCHAR(120),
     Senha NCHAR(32),
     Fk_Usuario_Id INT
 );
+
 -------------------------------------------------------------------------------------------
 
 --Tabelas de endereço
@@ -86,6 +87,22 @@ CREATE TABLE Imagem (
     url VARCHAR(120),
     fk_Imovel_ID INT
 );
+
+-------------------------------------------------------------------------------------------
+--Sessão de contato
+CREATE TABLE TipoContato(
+	Id INT PRIMARY KEY IDENTITY (1,1),
+	Nome VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE ContatoSite(
+	Id INT PRIMARY KEY IDENTITY (1,1),
+	Nome VARCHAR(80) NOT NULL,
+	Email VARCHAR(80) NOT NULL,
+	Mensagem TEXT NOT NULL,
+	DataHora Datetime NOT NULL,
+	fk_TipoContato_id INT
+);
 -------------------------------------------------------------------------------------------
 --Sessão dedicada aos Alter Tables para relacionamentos de entidades
 -- Usuário e login
@@ -94,7 +111,7 @@ ALTER TABLE Usuario ADD CONSTRAINT FK_Usuario_2
     REFERENCES TipoUsuario (ID)
     ON DELETE CASCADE;
 
-ALTER TABLE Login ADD CONSTRAINT FK_Login_2
+ALTER TABLE Login_Usuario ADD CONSTRAINT FK_Login_Usuario_2
     FOREIGN KEY (Fk_Usuario_Id)
     REFERENCES Usuario (ID);
 -------------------------------------------------------------------------------------------
@@ -126,13 +143,90 @@ ALTER TABLE Imovel ADD CONSTRAINT FK_Imovel_4
     REFERENCES TipoAnuncio (ID)
     ON DELETE CASCADE;
 
-/*
-ALTER TABLE Imovel ADD CONSTRAINT FK_Imovel_5
-    FOREIGN KEY (Imagem???)
-    REFERENCES ??? (???);
-*/
-
 ALTER TABLE Imagem ADD CONSTRAINT FK_Imagem_2
     FOREIGN KEY (fk_Imovel_ID)
     REFERENCES Imovel (ID);
 ----------------------------------------------------------------------------------------------
+--Contato
+alter table ContatoSite add constraint FK_ContatoSite_2
+	foreign key (fk_TipoContato_id)
+	references TipoContato (id)
+	on delete cascade;
+----------------------------------------------------------------------------------------------
+--PROCEDURES
+--INSERIR DADOS NAS TABELAS
+
+--TABELA TipoContato--------------------------------------------------------------------------
+IF OBJECT_ID('InserirDado', 'P') IS NOT NULL
+	DROP PROCEDURE InserirDado;
+GO
+
+--criando procedure
+CREATE PROCEDURE InserirDado
+@nome VARCHAR(80)
+AS -- inicia a procedure
+
+BEGIN -- início do bloco da procedure
+	INSERT INTO TipoContato(nome) VALUES (@nome);
+END; --fim do bloco dda procedure
+
+--chamando a procedure
+EXEC InserirDado @nome = 'Não encontrei o imóvel';
+EXEC InserirDado @nome = 'Não encontrei o vendedor';
+EXEC InserirDado @nome = 'Quero vender';
+EXEC InserirDado @nome = 'Crítica';
+EXEC InserirDado @nome = 'Sugestões';
+
+--TABELA ContatoSite---------------------------------------------------------------------------
+IF OBJECT_ID('InserirDadoContato', 'P') IS NOT NULL
+	DROP PROCEDURE InserirDadoContato;
+GO
+
+--criando procedure
+CREATE PROCEDURE InserirDadoContato
+@nome VARCHAR(80),
+@email  VARCHAR(80),
+@mensagem TEXT,
+@fk INT
+
+AS -- inicia a procedure
+
+BEGIN -- início do bloco da procedure
+	DECLARE @tipoContatoID INT;
+	SELECT @tipoContatoID = Id FROM TipoContato WHERE Id = @fk;
+
+	IF @tipoContatoID IS NULL
+	BEGIN
+		PRINT('Tipo de Contato não encontrado');
+		RETURN -1;
+	END
+
+	INSERT INTO ContatoSite(Nome, Email, Mensagem, DataHora, fk_TipoContato_id)
+	VALUES (@nome, @email, @mensagem, GETDATE(), @tipoContatoID);
+END;
+ --fim do bloco dda procedure
+
+--chamando a procedure
+exec InserirDadoContato @nome = 'José', @email = 'josfilho@email.com', @mensagem = 'Quais as formas de pagamento', @fk = 1;
+exec InserirDadoContato @nome = 'Maria Santos', @email = 'maria.santos@hotmail.com', @mensagem = 'Quero fazer uma reclamação sobre o atendimento', @fk = 2;
+exec InserirDadoContato @nome = 'Pedro Costa', @email = 'pedro.costa@yahoo.com', @mensagem = 'Parabéns pelo excelente trabalho', @fk = 3;
+exec InserirDadoContato @nome = 'Ana Lima', @email = 'ana.lima@outlook.com', @mensagem = 'Preciso de um orçamento para um projeto', @fk = 4;
+exec InserirDadoContato @nome = 'Bruno Dias', @email = 'bruno.dias@uol.com.br', @mensagem = 'Tenho uma dúvida sobre a forma de pagamento', @fk = 5;
+exec InserirDadoContato @nome = 'Carla Souza', @email = 'carla.souza@bol.com.br', @mensagem = 'Quero cancelar minha compra', @fk = 1;
+exec InserirDadoContato @nome = 'Daniel Oliveira', @email = 'daniel.oliveira@terra.com.br', @mensagem = 'Gostaria de fazer uma sugestão de melhoria', @fk = 2;
+exec InserirDadoContato @nome = 'Eduarda Rocha', @email = 'eduarda.rocha@icloud.com', @mensagem = 'Quero elogiar o atendimento do vendedor', @fk = 3;
+exec InserirDadoContato @nome = 'Fabio Martins', @email = 'fabio.martins@globo.com', @mensagem = 'Preciso de ajuda para resolver um problema', @fk = 4;
+exec InserirDadoContato @nome = 'Fabio Martins', @email = 'gabriela.alves@zoho.com', @mensagem = 'Quero fazer uma parceria com sua empresa', @fk = 5;
+
+----------------------------------------------------------------------------------------------
+-- SESSÃO DE MANUTENÇÃO
+--ALTERAR NOME DE TABELA
+exec sp_rename login_usuario, Login_Usuario ;
+--SELECIONAR TABELA
+select * from Login_Usuario;
+select * from TipoContato;
+select * from ContatoSite;
+
+--EXCLUIR TABELA
+DROP TABLE TipoContato;
+DROP TABLE ContatoSite;

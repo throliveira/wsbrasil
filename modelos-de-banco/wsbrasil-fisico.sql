5462cc535d517daf6b098d2752bddafcc66cf494
throliveira <135062106+throliveira@users.noreply.github.com>
2023-11-23 15:26:35 -0300

    Atividades do dia 23/11 - aula assíncrona

--- modelos-de-banco/wsbrasil-fisico.sql
+++ modelos-de-banco/wsbrasil-fisico.sql
@@ -641,6 +641,8 @@ JOIN TipoContato
 ON ContatoSite.fk_TipoContato_id = TipoContato.Id
 ;
 --VIEWS-----------------------------------------------------------------------------------------
+--Crie uma View para mostrar todos os detalhes do imóvel.---------------------------------------
+----Chame essa View mostrando os imóveis de acordo com a cidade que você informa.---------------
 CREATE VIEW vw_DetalhesImovel 
 AS
 SELECT i.ID, i.Valor_imovel, e.Logradouro, e.Complemento, e.Numero, b.Nome_bairro , c.Nome_cidade, u.Nome_UF, u.Sigla_UF
@@ -653,6 +655,81 @@ FROM Imovel i
 GO
 
 SELECT * FROM vw_DetalhesImovel;
+go
+--EXERCÍCIOS-23/11/2023-------------------------------------------------------------------------
+--Crie uma View para mostrar quantidade de imóveis por cidade.----------------------------------
+CREATE VIEW vw_QtdImovelPorCidade
+AS
+SELECT COUNT(i.ID) AS QtdImoveis
+FROM Imovel i
+	JOIN Endereco e ON i.fk_Endereco_ID = e.ID
+	JOIN Bairro b ON e.fk_Bairro_ID = b.ID
+	JOIN Cidade c ON b.fk_Cidade_ID = c.ID
+WHERE c.Nome_cidade = 'Guarulhos';
+GO
+
+SELECT * FROM vw_QtdImovelPorCidade;
+go
+--Crie uma View para mostrar a quantidade de imóveis por tipo de anúncio.-----------------------
+CREATE VIEW vw_QtdImovelPorTipoAnuncio
+AS
+SELECT t.Nome_Tipo_Anuncio, COUNT(i.ID) AS QtdImoveis, e.Logradouro, e.Complemento, e.Numero, b.Nome_bairro, c.Nome_cidade, u.Nome_UF, u.Sigla_UF 
+FROM Imovel i
+	JOIN Endereco e ON i.fk_Endereco_ID = e.ID
+	JOIN TipoAnuncio t on i.fk_TipoAnuncio_ID = t.ID
+	JOIN Bairro b ON e.fk_Bairro_ID = b.ID
+	JOIN Cidade c ON b.fk_Cidade_ID = c.ID
+	JOIN UF u ON c.fk_UF_ID = u.ID
+WHERE t.Nome_Tipo_Anuncio = 'vender'
+GROUP BY t.Nome_Tipo_Anuncio, e.Logradouro, e.Complemento, e.Numero, b.Nome_bairro, c.Nome_cidade, u.Nome_UF, u.Sigla_UF;
+GO
+
+SELECT * FROM vw_QtdImovelPorTipoAnuncio;
+go
+--Crie uma View para calcular a soma dos valores dos imóveis por tipo de imóvel.----------------
+CREATE VIEW vw_SomarValorImovelPorTipo
+AS
+SELECT SUM(i.Valor_imovel) AS SomaImovelValor, ti.NomeTipoImovel
+FROM Imovel i
+	JOIN TipoImovel ti ON i.fk_TipoImovel_ID = ti.ID
+WHERE ti.NomeTipoImovel = 'casa'
+GROUP BY ti.NomeTipoImovel;
+GO
+
+SELECT * FROM vw_SomarValorImovelPorTipo;
+--Crie uma procedure para deletar imóveis pela cidade.------------------------------------------
+IF OBJECT_ID('DeletarImoveisPorCidade', 'P') IS NOT NULL
+    DROP PROCEDURE DeletarImoveisPorCidade;
+GO
+
+CREATE PROCEDURE DeletarImoveisPorCidade
+    @nomeCidade VARCHAR(120)
+AS
+BEGIN
+    DECLARE @cidadeID INT;
+    SELECT @cidadeID = ID FROM Cidade WHERE Nome_cidade = @nomeCidade;
+
+    IF @cidadeID IS NULL
+    BEGIN
+        PRINT('Cidade não encontrada');
+        RETURN;
+    END
+
+    DELETE FROM Imovel
+    WHERE fk_Endereco_ID IN (
+        SELECT e.ID
+        FROM Endereco e
+        INNER JOIN Bairro b ON e.fk_Bairro_ID = b.ID
+        WHERE b.fk_Cidade_ID = @cidadeID
+    );
+
+    PRINT('Imóveis na cidade ' + @nomeCidade + ' foram deletados com sucesso.');
+END;
+go
+
+EXEC DeletarImoveisPorCidade @nomeCidade = 'Guarulhos';
+go
+------------------------------------------------------------------------------------------------
 -- SESSÃO DE MANUTENÇÃO
 --ALTERAR NOME DE TABELA
 exec sp_rename login_usuario, Login_Usuario ;
